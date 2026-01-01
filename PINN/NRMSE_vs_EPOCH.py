ab93e7a1ec9dc1493dcefc2dd4b50abe18b85cbe
divyesh-savaliya <51810737+divyesh-savaliyaa@users.noreply.github.com>
2026-01-01 13:51:11 +0100

    Update NRMSE_vs_EPOCH.py

--- PINN/NRMSE_vs_EPOCH.py
+++ PINN/NRMSE_vs_EPOCH.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("nrmse.csv")
+df = pd.read_csv("checkpoints/nrmse.csv")
 
 x = df["epoch"].to_numpy() / 1000.0
 
